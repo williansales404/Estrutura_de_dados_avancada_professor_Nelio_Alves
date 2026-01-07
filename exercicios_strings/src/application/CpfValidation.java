4a1ec17087e8f670f7ca3d378810ce26e75d1b5f
williansales404 <willian9542@gmail.com>
2026-01-07 19:08:56 -0300

    organizando pastas

--- exercicios_strings/src/application/CpfValidation.java
+++ exercicios_strings/src/application/CpfValidation.java
@@ -10,9 +10,25 @@ public class CpfValidation {
 		boolean valCpf = validationCpf(Verificationcpf, padrao);
 		System.out.println(valCpf == false ? "Valido" : "Invalido");
 		
+//		String str = "874092172-93";
+//		//Segunda solução criada por min
+//		System.out.println(clearCpf(str));
+		
 	}
 	public static String cpfVerification(String cpf) {
 		return cpf.replaceAll("\\D+", "");
+	}
+	
+	public static String clearCpf(String str) {
+		StringBuilder sb = new StringBuilder();
+		char[] arrayStr = str.toCharArray();
+		for(int i = 0; i < arrayStr.length; i++) {
+			int a = Character.getNumericValue(arrayStr[i]);
+			if(a >= 0) {
+				sb.append(a);
+			}
+		}
+		return sb.toString();
 	}
 	
 	public static boolean validationCpf(String padrao, String cpf) {
---
+++ exercicios_strings/src/application/DominioEmail.java
@@ -0,0 +1,23 @@
+package application;
+
+public class DominioEmail {
+
+	public static void main(String[] args) {
+		
+		String lista = "Nome,E-mail,Telefone,CPF,Data de nascimento\r\n"
+				+ "Rogério Águas Bezerra Filho,rogerioaguas@email.com,(49) 92361-2598,83085126406,22/07/1995\r\n"
+				+ "Cleberson Kauê Assunção Sobrinho,clebinho@email.com.br,(95) 92589-4243,18440126387,19/03/1959\r\n"
+				+ "Francisco Chico de Dias,chico@email.com.br,(67) 92846-3472,80544915143,17/10/2004\r\n"
+				+ "Bóris Thiago Carrara Mendes,borio@email.com,(85) 92657-6956,48275258979,17/06/1999\r\n"
+				+ "Benedito Nivaldo de Cruz,beneh@email.com,(44) 92891-2952,87769158168,15/12/1997\r\n"
+				+ "Anna Dara Brito,aninha@email.com,(64) 92967-8938,71642300721,04/03/1994\r\n"
+				+ "Suzana Galindo de Quintana,suzanaquintana@email.com,(28) 93481-2521,84658458594,19/11/1964\r\n"
+				+ "Jason Raul Burgos Leal de Azevedo,jason@email.com.br,(98) 93602-3624,826.580.743-01,13 05 1973\r\n"
+				+ "Julieta Correia Marin,julie@email.com,(13) 92977-7223,752.473.458-19,22111996\r\n"
+				+ "Berenice Sabrina Bezerra de Maldonado,berenice@email.com,(28) 3547-5161,764.682.144-67,28.03.1984\r\n";
+		
+		
+		
+	}
+
+}
